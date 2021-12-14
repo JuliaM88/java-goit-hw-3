0b0bcc733b5464f5bb9c1a6308c755a8e4ec0d7f
JuliaM88 <94804170+JuliaM88@users.noreply.github.com>
2021-12-14 17:28:15 +0100

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,4 +1,5 @@
-public int countBanknotes(int sum){
+public class ATM{
+    public int countBanknotes(int sum){
     int [] banknotes = new int []{500, 200, 100, 50, 20, 10 ,5,2,1};
     int qty=0;
     int i = 0;
@@ -13,4 +14,5 @@ public int countBanknotes(int sum){
         sum = sum - banknotes[i] * currentQty;
     }
     return qty;
+}
 }
